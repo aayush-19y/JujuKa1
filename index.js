c74b52d4055777125ece870f96aafbeab3c72730
aayush-19y <2021bcs103@sggs.ac.in>
2023-08-14 13:28:52 +0530

    First commit make jujutsu kaisen web now converting it to anime_clothes selling website

---
+++ index.js
@@ -0,0 +1,146 @@
+function init() {
+
+    gsap.registerPlugin(ScrollTrigger);
+    
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+    
+    locoScroll.on("scroll", ScrollTrigger.update);
+    
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, 
+        getBoundingClientRect() {
+            return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+        },
+        
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
+    
+    
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+}
+
+init() 
+
+var crsr = document.querySelector(".cursor");
+var main = document.querySelector(".main");
+document.addEventListener("mousemove", function(dets){
+    // console.log(dets);
+    crsr.style.left = dets.x +10+ "px";
+    crsr.style.top = dets.y +10+ "px";
+
+}) 
+
+// var vd = document.querySelector("video");
+// vd.addEventListener("mouseenter", function(dets){
+// // console.log("Enter video");
+
+// })
+
+
+var tl = gsap.timeline({
+    scrollTrigger : {
+        trigger : ".page1 h1",
+        scroller : ".main",
+        // markers:true,
+        start : "top 8",
+        end : "top 0",
+        scrub:3
+    }
+})
+
+
+tl.to(".page1 h1",{
+    x:-80,
+    // duration:1,
+},"varsame")
+
+tl.to(".page1 h2",{
+    x:80
+},"varsame")
+
+tl.to(".page1 video",{
+    width:"80%"
+},"varsame")
+
+
+var tl2 = gsap.timeline({
+    scrollTrigger : {
+        trigger : ".page1 h1",
+        scroller : ".main",
+        // markers:true,
+        start : "top -115%",
+        end : "top -120%",
+        scrub:3
+    }
+})
+
+
+tl2.to(".main",{
+    backgroundColor: "#fff"
+})
+
+
+var tl3 = gsap.timeline({
+    scrollTrigger : {
+        trigger : ".page1 h1",
+        scroller : ".main",
+        // markers:true,
+        start : "top -320%",
+        end : "top -322%",
+        scrub:3
+    }
+})
+
+tl3.to(".main",{
+    backgroundColor:"#0F0D0D" ,
+})
+
+var boxes = document.querySelectorAll(".box");
+// console.log(boxes);
+boxes.forEach(function(ele){
+    // console.log(ele)
+    ele.addEventListener("mouseenter",function(){
+        // ele.style.backgroundColor = "red"
+        var att = ele.getAttribute("data-image")
+        console.log(att)
+        crsr.style.width = "350px"
+        crsr.style.height = "300px"
+        crsr.style.borderRadius = "0"
+        crsr.style.backgroundImage = `url(${att})`
+      
+
+    })
+
+    ele.addEventListener("mouseleave",function(){
+        ele.style.backgroundColor = "transparent"
+        crsr.style.width = "16px"
+        crsr.style.height = "16px"
+        crsr.style.borderRadius = "50%"
+        crsr.style.backgroundImage = `none`
+    })
+})
+
+var purple = document.querySelector("#purple")
+var h4 = document.querySelectorAll("#nav h4")
+h4.forEach(function(elem){
+    // console.log(elem);
+    elem.addEventListener("mouseenter",function(){
+        // console.log(elem)  b 
+        purple.style.display = "block"
+        purple.style.opacity = "1"
+    })
+
+    document.querySelector("#nav").addEventListener("mouseleave",function(){
+        // console.log(elem)
+        purple.style.display = "none"
+        purple.style.opacity = "0"
+    })
+})
